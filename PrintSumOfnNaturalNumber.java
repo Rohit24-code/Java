cb95ee4dbfac2cda91d35bdabb9d25df3bdc5836
Rohit singh <rohitsingh17031995@gmail.com>
2023-02-04 11:34:13 +0530

    Add files via upload

---
+++ PrintSumOfnNaturalNumber.java
@@ -0,0 +1,10 @@
+public class PrintSumOfnNaturalNumber {
+    public static void main(String[] args){
+         int num=4;
+         int sum=0;
+         for(int i=1;i<=num;i++){
+           sum+=i;
+         }
+         System.out.println(sum);
+    }
+}
