47985339f549dafdb83b2f3b998badb9ded644da
Rohit singh <rohitsingh17031995@gmail.com>
2023-02-04 11:35:43 +0530

    Add files via upload

---
+++ PrintTableOfNumber.java
@@ -0,0 +1,8 @@
+public class PrintTableOfNumber {
+    public static void main(String[] args){
+        int num=5;
+        for(int i=1;i<=10;i++){
+            System.out.println(num*i);
+        }
+    }
+}
