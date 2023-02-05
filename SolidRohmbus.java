4dd9d6982c70d7736cce3c43b9238ddb0d2673e2
Rohit singh <rohitsingh17031995@gmail.com>
2023-02-05 08:59:54 +0530

    Add files via upload

---
+++ SolidRohmbus.java
@@ -0,0 +1,16 @@
+public class SolidRohmbus {
+    public static void main(String[] args){
+        int n=5;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<(n+n-i-1);j++){
+                if(j>=n-i-1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
